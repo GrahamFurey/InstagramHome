62b7c0a3695bca07598f496196f97792ec673ca3
Graham Furey <fureygraham@gmail.com>
2021-08-17 19:33:10 -0400

    Initial Commit

---
+++ InstagramHomeScreen/InstagramHomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramHomeScreenApp.swift
+//  InstagramHomeScreen
+//
+//  Created by Graham Furey on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramHomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
